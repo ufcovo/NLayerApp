76b15cfc4a9df37050cf8377cc3ae00cac2f61c3
Mert <79603812+ufcovo@users.noreply.github.com>
2023-09-10 14:39:12 +0300

    Seed Data

--- NLayer.Repository/AppDbContext.cs
+++ NLayer.Repository/AppDbContext.cs
@@ -9,10 +9,10 @@ namespace NLayer.Repository
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-               
+
         }
 
-        public DbSet<Category> Categories{ get; set; }
+        public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductFeature> ProductFeatures { get; set; }
 
@@ -20,6 +20,25 @@ namespace NLayer.Repository
         {
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
             //modelBuilder.ApplyConfiguration(new ProductConfiguration());
+
+            modelBuilder.Entity<ProductFeature>().HasData(
+                new ProductFeature
+                {
+                    Id = 1,
+                    Color = "Red",
+                    Height = 100,
+                    Width = 100,
+                    ProductId = 1
+                },
+                new ProductFeature
+                {
+                    Id = 2,
+                    Color = "Blue",
+                    Height = 200,
+                    Width = 200,
+                    ProductId = 2
+                }
+            );
             base.OnModelCreating(modelBuilder);
         }
     }
---
+++ NLayer.Repository/Seeds/ProductSeed.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Seeds
+{
+    internal class ProductSeed : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasData(
+                new Product
+                {
+                    Id = 1,
+                    CategoryId = 1,
+                    Name = "Pencil 1",
+                    Price = 100,
+                    Stock = 100,
+                    CreatedDate = DateTime.Now
+                },
+                 new Product
+                 {
+                     Id = 2,
+                     CategoryId = 1,
+                     Name = "Pencil 2",
+                     Price = 200,
+                     Stock = 200,
+                     CreatedDate = DateTime.Now
+                 },
+                  new Product
+                  {
+                      Id = 3,
+                      CategoryId = 1,
+                      Name = "Pencil 3",
+                      Price = 300,
+                      Stock = 300,
+                      CreatedDate = DateTime.Now
+                  },
+                   new Product
+                   {
+                       Id = 4,
+                       CategoryId = 2,
+                       Name = "Book 1",
+                       Price = 100,
+                       Stock = 100,
+                       CreatedDate = DateTime.Now
+                   },
+                    new Product
+                    {
+                        Id = 1,
+                        CategoryId = 2,
+                        Name = "Book 2",
+                        Price = 200,
+                        Stock = 200,
+                        CreatedDate = DateTime.Now
+                    }
+            );
+        }
+    }
+}
