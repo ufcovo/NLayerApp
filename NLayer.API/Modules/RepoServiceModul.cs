8da86d96a45f5905f0121c86c0dca8fb8939822b
Mert <79603812+ufcovo@users.noreply.github.com>
2023-09-12 21:45:12 +0300

    Caching Implementation

--- NLayer.API/Modules/RepoServiceModul.cs
+++ NLayer.API/Modules/RepoServiceModul.cs
@@ -1,4 +1,5 @@
 ﻿using Autofac;
+using NLayer.Caching;
 using NLayer.Core.Repositories;
 using NLayer.Core.Services;
 using NLayer.Core.UnitOfWorks;
@@ -39,7 +40,7 @@ namespace NLayer.API.Modules
                 .AsImplementedInterfaces()
                 .InstancePerLifetimeScope();
 
-
+            builder.RegisterType<ProductServiceCaching>().As<IProductService>();
 
         }
     }
